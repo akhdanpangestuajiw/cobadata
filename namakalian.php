3389cbefc64f754c7a59e44bedb5d1fb969271e1
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-03-11 12:05:22 +0700

    Initial commit

---
+++ namakalian.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TP 01</title>
+</head>
+<body>
+	<?php 
+		echo "Halo, Ini adalah Tugas TP 01<br>";
+		echo "Nama : Akhdan Pangestuaji Widodo<br>";
+		echo "NIM  : 6706190008<br>";
+		echo "Kelas: D3RPLA-43-02<br>";
+		echo "Ttl  : Purwokerto,Banyumas 13 Maret 2001<br>";
+		echo "No.Wa:081328541502<br>";
+		echo "Motivasi : Nikah biayanya mahal, jadi jangan malas yah !";
+
+	 ?>
+</body>
+</html>
